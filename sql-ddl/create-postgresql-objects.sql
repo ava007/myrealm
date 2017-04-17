8ce2155df658b7b514c2b0dbb50e281501c1e876
ava007 <avonarx@gmx.net>
2017-04-17 18:07:38 +0200

    added Image Table

--- sql-ddl/create-postgresql-objects.sql
+++ sql-ddl/create-postgresql-objects.sql
@@ -11,3 +11,24 @@ with (oids = false);
 
 create unique index myr_person_email on myr_person(email);
 create unique index myr_person_nick on myr_person(nick);
+
+
+create table myr_image(
+  id character(36) primary key,
+  person_id character(36) not null,
+  created  timestamp with time zone DEFAULT ('now'::text)::timestamp with time zone,
+  modified timestamp with time zone DEFAULT ('now'::text)::timestamp with time zone,
+  img_title character varying (80) not null,
+  img_description character varying(250),
+  img_filesize integer,
+  img_internalfilename character varying(40),
+  img_type character varying(12),
+  img_height integer,
+  img_width integer
+) 
+with (oids = false);
+
+ALTER TABLE ONLY myr_image
+    ADD CONSTRAINT image_person_fk FOREIGN KEY (person_id) REFERENCES myr_person(id);
+
+
