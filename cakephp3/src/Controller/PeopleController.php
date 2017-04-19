126b79d2d1ca2c7dc0fdde5adac16f595701323d
ava007 <avonarx@gmx.net>
2017-04-19 19:32:46 +0200

    Create PeopleController.php

---
+++ cakephp3/src/Controller/PeopleController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\ORM\TableRegistry;
+use Cake\Datasource\ConnectionManager;
+use Cake\Network\Email\Email;
+
+class PeopleController extends AppController
+{
+  public function register() {
+
+  }
+}
