973ba0e7098725e855061bc146c111c8ff16d565
ava007 <avonarx@gmx.net>
2017-04-20 20:35:14 +0200

    Update app.php

--- cakephp3/config/app.php
+++ cakephp3/config/app.php
@@ -41,7 +41,7 @@ return [
         'encoding' => env('APP_ENCODING', 'UTF-8'),
         'defaultLocale' => env('APP_DEFAULT_LOCALE', 'en_US'),
         'base' => false,
-        'dir' => 'cakephp3',
+        'dir' => 'src',
         'webroot' => 'webroot',
         'wwwRoot' => WWW_ROOT,
         // 'baseUrl' => env('SCRIPT_NAME'),
